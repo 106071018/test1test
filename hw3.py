7f5c9d801d6ad0de041a39b997d70f0c7da23bd3
106071018 <37101243+106071018@users.noreply.github.com>
2018-03-27 16:42:31 +0800

    first.commit

---
+++ hw3.py
@@ -0,0 +1,24 @@
+# test1test
+# coding: utf-8
+
+# In[1]:
+
+
+def aaa(x):
+    bbb=dict()
+    for ccc in x:
+        if ccc not in bbb:
+            bbb[ccc]=1
+        else:
+            bbb[ccc]+=1
+    return bbb
+
+
+# In[16]:
+
+
+sen=input('Please type anything:''\n')
+for item in aaa(sen).items():
+    zzz,xxx=item
+    print("'"+str(zzz)+"' = '"+str(xxx)+"'")
+
